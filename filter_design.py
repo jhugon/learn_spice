804fdd60ec45fc978a81e5e2eec3d9abb30fe57a
Justin Hugon <jhugon@users.noreply.github.com>
2022-03-23 15:38:29 -0500

    filter_design: updated printing behavior

--- filter_design.py
+++ filter_design.py
@@ -6,8 +6,8 @@ from matplotlib import colors as mcolors
 import numpy as np
 
 def plot_filters_behavior(systems,labels,title,savename,t_max=7,f_min=1e-3,f_max=1e3):
-    N_ts = 200
-    N_ws = 200
+    N_ts = 500
+    N_ws = 500
     ws = np.logspace(np.log10(f_min),np.log10(f_max),N_ws)
     ts = np.linspace(0,t_max,N_ts)
 
@@ -56,15 +56,92 @@ def plot_filters_behavior(systems,labels,title,savename,t_max=7,f_min=1e-3,f_max
 
     fig.savefig(savename)
 
+    print("Frequency Response:")
     for label,y_impulse,y_step,w,mag,phase in system_results:
-        max_impulse = y_impulse.max()
-        iMax_impulse = y_impulse.argmax()
-        max_step = y_step.max()
-        t10pct_step = ts[y_step >= max_step*0.1][0]
-        t90pct_step = ts[y_step >= max_step*0.9][0]
-        t100pct_impulse = ts[y_impulse == max_impulse][0]
-        t10pct_impulse = ts[iMax_impulse:][y_impulse[iMax_impulse:] <= max_impulse*0.1][0]
-        print(f"{label:50} impulse 0-100%: {t100pct_impulse:5.2f} 100-10%: {t10pct_impulse-t100pct_impulse:5.2f} max: {max_impulse:4.2f} step 10-90%: {t90pct_step-t10pct_step:5.2f} max: {max_step:4.2f}")
+        w_3db = float('nan')
+        f_3db = float('nan')
+        w_m180 = float('nan')
+        f_m180 = float('nan')
+        mag_phase_m180 = float('nan')
+        gain_margin = float('nan')
+        try:
+            w_3db = ws[mag <= mag[0]-3.][0]
+            f_3db = w_3db/2/np.pi
+        except IndexError:
+            pass
+        try:
+            w_m180 = ws[phase <= -180.][0]
+            f_m180 = w_m180/2/np.pi
+        except IndexError:
+            pass
+        try:
+            mag_phase_m180 = mag[phase <= -180.][0]
+            gain_margin = -mag_phase_m180
+        except IndexError:
+            pass
+        print(f"{label:50} -3db: {w_3db:8.3g} rad/s = {f_3db:8.3g} Hz,  phase<=180 @ {w_m180:8.3g} rad/s = {f_m180:8.3g} Hz,  gain margin: {gain_margin:4.1f} dB")
+    print("Impulse Response:")
+    for label,y_impulse,y_step,w,mag,phase in system_results:
+        max_impulse = float('nan')
+        min_impulse = float('nan')
+        iMax_impulse = float('nan')
+        tMax_impulse = float('nan')
+        t10pct_impulse = float('nan')
+        t100pct_impulse = float('nan')
+        t10pct_fall_impulse = float('nan')
+        fwhm_impulse = float('nan')
+        try:
+            max_impulse = y_impulse.max()
+            min_impulse = y_impulse.min()
+            iMax_impulse = y_impulse.argmax()
+        except ValueError:
+            pass
+        try:
+            tMax_impulse = ts[iMax_impulse]
+        except IndexError:
+            pass
+        try:
+            t10pct_impulse = ts[:iMax_impulse][y_impulse[:iMax_impulse] <= max_impulse*0.1][-1]
+        except IndexError:
+            pass
+        try:
+            t100pct_impulse = ts[y_impulse == max_impulse][0]
+        except IndexError:
+            pass
+        try:
+            t10pct_fall_impulse = ts[iMax_impulse:][y_impulse[iMax_impulse:] <= max_impulse*0.1][0]
+        except IndexError:
+            pass
+        try:
+            rise_half_impulse = ts[:iMax_impulse][y_impulse[:iMax_impulse] <= max_impulse*0.5][-1]
+            fall_half_impulse = ts[iMax_impulse:][y_impulse[iMax_impulse:] <= max_impulse*0.5][0]
+            fwhm_impulse = fall_half_impulse-rise_half_impulse
+        except IndexError:
+            pass
+        print(f"{label:50} peak delay: {tMax_impulse:8.3g}  10-100%: {t100pct_impulse-t10pct_impulse:8.3g}  100-10%: {t10pct_fall_impulse-t100pct_impulse:5.3g}  max: {max_impulse:5.3g}  min: {min_impulse:4.3g}  FWHM: {fwhm_impulse:8.5g}")
+    print("Step Response:")
+    for label,y_impulse,y_step,w,mag,phase in system_results:
+        max_step = float('nan')
+        overshoot = float('nan')
+        t10pct_step = float('nan')
+        t90pct_step = float('nan')
+        try:
+            max_step = y_step.max()
+        except ValueError:
+            pass
+        try:
+            overshoot = max_step - y_step[-1]
+        except IndexError:
+            pass
+        try:
+            t10pct_step = ts[y_step >= 0.1*y_step[-1]][0]
+        except IndexError:
+            pass
+        try:
+            t90pct_step = ts[y_step >= 0.9*y_step[-1]][0]
+        except IndexError:
+            pass
+        print(f"{label:50} 10-90%: {t90pct_step-t10pct_step:8.4g}  overshoot: {overshoot:8.4g}")
 
 def semi_gaussian_complex_pole_locations(N,out_img_fn=None):
     """
@@ -135,12 +212,18 @@ def semi_gaussian_complex_pole_locations(N,out_img_fn=None):
 
 def semi_gaussian_complex_all_pole_filter(N,out_img_fn=None):
     poles = semi_gaussian_complex_pole_locations(N)
-    if N == 3:
-        poles *= 0.83
+    if N == 2:
+        poles *= 0.8657314629
+    elif N == 3:
+        poles *= 1.482965932
     elif N == 4:
-        poles *= 0.75
+        poles *= 1.955911824
     elif N == 5:
-        poles *= 1.36
+       poles *= 2.340681363
+    if N % 2 == 0:
+        poles = np.hstack([poles,poles.conj()])
+    else:
+        poles = np.hstack([poles[:-1],poles[:-1].conj(),poles[-1]])
     f = signal.ZerosPolesGain([],poles,[1])
     _, resp = f.freqresp([1e-9])
     scale_factor = 1./abs(resp)
@@ -178,9 +261,9 @@ if __name__ == "__main__":
         (real_all_pole_filter,"Real All-Pole Filter"),
         (complex_pole_filter,"Complex Pole Filter"),
         (semi_gaussian_C3_filter,"Semi-Gaussian C3 Filter (from paper)"),
-        #(semi_gaussian_complex_all_pole_filter(2),"Semi-Gaussian C2 Filter"),
+        (semi_gaussian_complex_all_pole_filter(2),"Semi-Gaussian C2 Filter"),
         (semi_gaussian_complex_all_pole_filter(3),"Semi-Gaussian C3 Filter"),
         (semi_gaussian_complex_all_pole_filter(4),"Semi-Gaussian C4 Filter"),
         (semi_gaussian_complex_all_pole_filter(5),"Semi-Gaussian C5 Filter"),
     ]
-    plot_filters_behavior([x[0] for x in filters_to_plot],[x[1] for x in filters_to_plot],"Filter Design","Filter_Design.pdf",t_max=7)
+    plot_filters_behavior([x[0] for x in filters_to_plot],[x[1] for x in filters_to_plot],"Filter Design","Filter_Design.pdf",t_max=4)
